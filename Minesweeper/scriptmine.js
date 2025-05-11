eee2bab81f777fbb0dd278e17313a0c76142aea2
swyxt15 <122916001+swyxt15@users.noreply.github.com>
2025-05-11 12:36:49 +0200

    Update scriptmine.js

--- Minesweeper/scriptmine.js
+++ Minesweeper/scriptmine.js
@@ -114,3 +114,21 @@ window.addEventListener('load', () => {
     });
 });
 
+
+
+function changeBackgroundColor() {
+    let bodyElement = document.body;
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    bodyElement.style.backgroundColor = randomColor;
+}
+
+
+
+window.addEventListener('load', () => {
+    document.body.classList.add('loaded');
+    
+    const overlay = document.getElementById('overlay');
+    overlay.addEventListener('transitionend', () => {
+        overlay.remove(); 
+    });
+});
