9e3e8424c4e6e22a86eeacaca1a45b511db85669
swyxt15 <122916001+swyxt15@users.noreply.github.com>
2025-05-11 12:00:59 +0200

    Add files via upload

---
+++ Pong/scriptpong.js
@@ -0,0 +1,106 @@
+const canvas = document.getElementById('pongCanvas');
+const ctx = canvas.getContext('2d');
+
+
+const paddleWidth = 10;
+const paddleHeight = 100;
+const ballRadius = 10;
+let upArrowPressed = false;
+let downArrowPressed = false;
+let paddleY = canvas.height / 2 - paddleHeight / 2;
+let ballX = canvas.width / 2;
+let ballY = canvas.height / 2;
+let ballSpeedX = 4;
+let ballSpeedY = 4;
+let aiPaddleY = canvas.height / 2 - paddleHeight / 2;
+const aiSpeed = 3;
+
+
+function drawPaddle(x, y) {
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(x, y, paddleWidth, paddleHeight);
+}
+
+function drawBall(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function update() {
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+
+    if (ballY + ballRadius > canvas.height || ballY - ballRadius < 0) {
+        ballSpeedY = -ballSpeedY;
+    }
+
+    if (ballX - ballRadius < paddleWidth) {
+        if (ballY > paddleY && ballY < paddleY + paddleHeight) {
+            ballSpeedX = -ballSpeedX;
+        } else if (ballX - ballRadius < 0) {
+            resetBall();
+        }
+    } else if (ballX + ballRadius > canvas.width - paddleWidth) {
+        if (ballY > aiPaddleY && ballY < aiPaddleY + paddleHeight) {
+            ballSpeedX = -ballSpeedX;
+        } else if (ballX + ballRadius > canvas.width) {
+            resetBall();
+        }
+    }
+
+
+    if (upArrowPressed && paddleY > 0) {
+        paddleY -= 7;
+    } else if (downArrowPressed && paddleY < canvas.height - paddleHeight) {
+        paddleY += 7;
+    }
+
+    if (aiPaddleY + paddleHeight / 2 < ballY) {
+        aiPaddleY += aiSpeed;
+    } else {
+        aiPaddleY -= aiSpeed;
+    }
+}
+
+function resetBall() {
+    ballX = canvas.width / 2;
+    ballY = canvas.height / 2;
+    ballSpeedX = -ballSpeedX;
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle(0, paddleY);
+    drawPaddle(canvas.width - paddleWidth, aiPaddleY);
+    drawBall(ballX, ballY);
+}
+
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') {
+        upArrowPressed = true;
+    } else if (e.key === 'ArrowDown') {
+        downArrowPressed = true;
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'ArrowUp') {
+        upArrowPressed = false;
+    } else if (e.key === 'ArrowDown') {
+        downArrowPressed = false;
+    }
+});
+
+gameLoop();
