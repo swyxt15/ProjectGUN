459f18526f505dbeaf88829be598f4cf879c233d
swyxt15 <122916001+swyxt15@users.noreply.github.com>
2025-05-11 12:05:15 +0200

    Update scriptest.js

--- scriptest.js
+++ scriptest.js
@@ -1,34 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const overlay = document.createElement('div');
-  overlay.id = 'transition-overlay';
-  document.body.appendChild(overlay);
-  
-  document.querySelectorAll('.cell a').forEach(link => {
-      link.addEventListener('click', function(e) {
-          e.preventDefault();  
-          overlay.classList.add('active');
-          setTimeout(() => {
-              window.location.href = link.href;
-          }, 500); 
-      });
-  });
-});
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
-    const logo = document.querySelector(".logo-header");
-    const sideMenu = document.getElementById("side-menu");
+  document.querySelectorAll('.cell a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
 
-    logo.addEventListener("click", function (event) {
-        event.stopPropagation(); 
-        sideMenu.classList.toggle("menu-open"); 
-    });
+      let overlay = document.createElement('div');
+      overlay.id = 'transition-overlay';
+      document.body.appendChild(overlay);
+      overlay.classList.add('active');
 
-   
-    document.addEventListener("click", function (event) {
-        if (!sideMenu.contains(event.target) && !logo.contains(event.target)) {
-            sideMenu.classList.remove("menu-open");
-        }
+      setTimeout(() => {
+        window.location.href = link.href;
+      }, 500);
     });
+  });
+
+  const logo = document.querySelector(".logo-header");
+  const sideMenu = document.getElementById("side-menu");
+
+  logo.addEventListener("click", function (event) {
+    event.stopPropagation();
+    sideMenu.classList.toggle("menu-open");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!sideMenu.contains(event.target) && !logo.contains(event.target)) {
+      sideMenu.classList.remove("menu-open");
+    }
+  });
 });
